6d5873f234a4dd69c0cfe6d9abfd46b72009cda2
Robert Alexander <roalexan@microsoft.com>
2022-03-25 17:57:20 -0400

    reorder console output a bit

--- deploy.sh
+++ deploy.sh
@@ -29,22 +29,23 @@ echo "ALIAS: ${ALIAS}"
 ## VARIABLES
 
 RESOURCE_GROUP="${PREFIX}usbcamerademorg" # name of the new resource group to create
+VM_NAME="${PREFIX}usbcamerademovm"
 # https://askubuntu.com/questions/1263020/what-is-the-azure-vm-image-urn-for-ubuntu-server-20-04-lts
 # syntax: publisher:offer:sku:version (shown in vm arm template)
-VM_USER_NAME="azureuser" # login user name of the virtual machine
 VM_IMAGE="Canonical:0001-com-ubuntu-server-focal:20_04-lts:latest" # Ubuntu Server 20.04 LTS - Gen 2
 # VM_IMAGE="Canonical:UbuntuServer:18_04-lts-gen2:latest" # Ubuntu Server 20.04 LTS - Gen 2
-VM_NAME="${PREFIX}usbcamerademovm"
 # https://azure.microsoft.com/en-us/pricing/details/virtual-machines/linux/
 # VM_SIZE="standard_ds3_v2" # Standard DS3 v2 (4 vcpus, 14 GiB memory)
-VM_SIZE="standard_ds4_v2"
+VM_SIZE="standard_ds4_v2" # Standard DS4 v2 (8 vcpus, 28 GiB memory)
+VM_USER_NAME="azureuser" # login user name of the virtual machine
 KEY_NAME="sshkey"
 
 echo "variables:"
 echo "RESOURCE_GROUP: ${RESOURCE_GROUP}"
-echo "VM_USER_NAME: ${VM_USER_NAME}"
+echo "VM_NAME: ${VM_NAME}"
 echo "VM_IMAGE: ${VM_IMAGE}"
 echo "VM_SIZE: ${VM_SIZE}"
+echo "VM_USER_NAME: ${VM_USER_NAME}"
 echo "KEY_NAME: ${KEY_NAME}"
 
 # SOURCE FILES
